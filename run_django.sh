aa7222788e7e87defc0eb0c2caee1bc2aa658d15
Stephen Moore <srmoore@gmail.com>
2019-03-14 11:39:04 -0400

    Fix IOT env vars. Update brain software to handle persistant storage

--- run_django.sh
+++ run_django.sh
@@ -2,8 +2,8 @@
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ## IOT SETTINGS
-export IOT_PRIVATE_KEY=${PROJECT_ROOT}/data/registration/rsa_private.pem
-export CA_CERTS=${PROJECT_ROOT}/data/registration/roots.pem
+export IOT_PRIVATE_KEY=/data/registration/rsa_private.pem
+export CA_CERTS=/data/registration/roots.pem
 export GCLOUD_PROJECT=openag-v1
 export GCLOUD_REGION=us-central1
 export GCLOUD_DEV_REG=device-registry
@@ -17,5 +17,5 @@ echo "Setting ENV vars"
 source ${PROJECT_ROOT}/set_env_vars.sh
 
 # source ${PROJECT_ROOT}/scripts/install/activate.sh
-busybox httpd -p 8088 -h ${PROJECT_ROOT}/data/images/
+# busybox httpd -p 8088 -h ${PROJECT_ROOT}/data/images/
 python3.6 manage.py runserver 0.0.0.0:80
