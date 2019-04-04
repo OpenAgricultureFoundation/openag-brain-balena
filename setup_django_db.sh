075904806651457181c339f5f628cb6f525ed62a
Stephen Moore <srmoore@gmail.com>
2019-04-04 09:45:53 -0400

    Added Balena specific ENVs that are required to Dockerfile.template so we don't have to set them on the 'Application' Made setup_django_db also check that the /data/images directory exists

--- setup_django_db.sh
+++ setup_django_db.sh
@@ -2,9 +2,19 @@
 
 cd /opt/openagbrain
 
-DB_LOCATION="${STORAGE_LOCATION:-data}/db"
+DATA_LOCATION="${STORAGE_LOCATION:-data}"
+
+DB_LOCATION="${DATA_LOCATION}/db"
+IMAGE_LOCATION="${DATA_LOCATION}/images"
 
 echo "DB LOCATION: ${DB_LOCATION}"
+echo "IMAGES LOCATION: ${IMAGE_LOCATION}"
+
+# Make sure images path exists
+if [[ ! -d "${IMAGE_LOCATION}" ]]; then
+    echo "Creating images directory"
+    mkdir -p ${IMAGE_LOCATION}
+fi
 
 if [[ ! -d "${DB_LOCATION}" ]]; then
     echo "Running First Time DB Setup"
@@ -17,9 +27,6 @@ if [[ ! -d "${DB_LOCATION}" ]]; then
     # Create the users
     echo "from django.contrib.auth.models import User; User.objects.filter(email='openag@openag.edu').delete(); User.objects.create_superuser('openag', 'openag@openag.edu', 'openag')" | python3.6 manage.py shell
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('backdoor', 'openag@openag.edu', 'B@ckd00r')" | python3.6 manage.py shell
-
-    # Collect the static files
-    # python3.6 manage.py collectstatic --clear --link --noinput
 
 else
     # We should run the migrate anyway, just in case data model has changed.
