6d86cb3e569e4c00b2e7024f251dc741b9b63112
Stephen Moore <srmoore@gmail.com>
2019-09-02 20:51:33 -0400

    Updates in general

--- run_django.sh
+++ run_django.sh
@@ -4,7 +4,11 @@ PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Stop the mdns responder till things settle down
 # python3.6 ${PROJECT_ROOT}/scripts/network/stop_mdns.py
 
-if [[ ! -f /data/network.configured ]]; then
+cd ${PROJECT_ROOT}
+echo "Setting up DB"
+${PROJECT_ROOT}/setup_django_db.sh
+
+if [[ ! -f /data/network.configured  && -f /data/system.configured ]]; then
     export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
     python scripts/network/clear_network_configs.py
     echo "Setting up wifi connection. Connect to the PFC-EDU-${RESIN_DEVICE_NAME_AT_INIT} access point"
@@ -21,10 +25,6 @@ if [[ "${HOSTNAME}" != "pfc-edu-${RESIN_DEVICE_NAME_AT_INIT}" ]]; then
     curl -X PATCH --header "Content-Type:application/json" --data "{\"network\": {\"hostname\": \"pfc-edu-${RESIN_DEVICE_NAME_AT_INIT}\"}}" "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
 fi
 
-cd ${PROJECT_ROOT}
-
-echo "Setting up DB"
-${PROJECT_ROOT}/setup_django_db.sh
 
 ## IOT SETTINGS
 export IOT_PRIVATE_KEY=/data/registration/rsa_private.pem
@@ -48,6 +48,14 @@ pip install -r requirements.txt
 echo "Setting ENV vars"
 source ${PROJECT_ROOT}/set_env_vars.sh
 
+# If we made it here, and the 'system.configured' file doesn't exist, this is the end of first boot, so make it.
+if [[ ! -f /data/system.configured ]]; then
+  touch /data/system.configured
+  # also create the network.configured file because we have network
+  # TODO: Make a ping check here
+  touch /data/network.configured
+fi
+
 # source ${PROJECT_ROOT}/scripts/install/activate.sh
 # busybox httpd -p 8088 -h ${PROJECT_ROOT}/data/images/
 if [[ ($DEBUGMODE == "true") ]]; then
@@ -57,4 +65,4 @@ fi
 # Remove the restart_mdns for now since it doesn't always work fully
 # python3.6 scripts/network/restart_mdns.py
 
-python3.6 manage.py runserver 0.0.0.0:80 # & python3.6 scripts/platform/rpi_button_handler.py
+python3.6 manage.py runserver 0.0.0.0:80 & python3.6 scripts/platform/rpi_button_handler.py
